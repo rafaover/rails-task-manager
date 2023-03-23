4752198785c323f51f6fb6c4aba460330c531802
Rafael Moreira <rafamsilva@gmail.com>
2023-03-23 19:48:38 +1100

    Added task new+create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#details", as: :task_details
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task_show
+  post "tasks", to: "tasks#create", as: :create_task
 end
