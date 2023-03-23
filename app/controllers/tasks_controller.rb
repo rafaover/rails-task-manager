62099738237778235e3408589300de6aaeca79d2
Rafael Moreira <rafamsilva@gmail.com>
2023-03-23 19:48:59 +1100

    updated controller with new and create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,23 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def details
+  def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
