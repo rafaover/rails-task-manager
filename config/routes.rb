3a4f24024aaba866bafe1c8ced136754d08a06b0
Rafael Moreira <rafamsilva@gmail.com>
2023-03-23 20:09:59 +1100

    route updated with tasks#edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task_show
   post "tasks", to: "tasks#create", as: :create_task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
 end
