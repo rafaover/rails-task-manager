e2fd20c1f01418c5883fdd9242053b36b016fbf6
Rafael Moreira <rafamsilva@gmail.com>
2023-03-24 12:33:29 +1100

    task controller: Edited create and update action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,12 +13,24 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to tasks_path(@task)
+    if @task.save
+      redirect_to tasks_path(@task)
+    else
+      render 'new'
+    end
   end
 
   def edit
     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path(@task)
+    else
+      render 'edit'
+    end
   end
 
   private
