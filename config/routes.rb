920cde3dd2e7349d2ba2a67036257ec85c8dfc40
Rafael Moreira <rafamsilva@gmail.com>
2023-03-24 14:04:55 +1100

    refac: added resources to router

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new", as: :new_task
-  get "tasks/:id", to: "tasks#show", as: :task_show
-  post "tasks", to: "tasks#create", as: :create_task
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update", as: :update_task
-  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # get "tasks/:id", to: "tasks#show", as: :task_show
+  # post "tasks", to: "tasks#create", as: :create_task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update", as: :update_task
+  # delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
+  resources :tasks
 end
