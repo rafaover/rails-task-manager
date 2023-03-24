177493e5273ce2afa2a05a341b040329fc5a5b5b
Rafael Moreira <rafamsilva@gmail.com>
2023-03-24 12:35:26 +1100

    dev: added update route on route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "tasks/:id", to: "tasks#show", as: :task_show
   post "tasks", to: "tasks#create", as: :create_task
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-
+  patch "tasks/:id", to: "tasks#update", as: :update_task
 end
